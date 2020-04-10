dcd7764ae64072b1f8aaf9be503eb7ded53e8b5b
Blue Banana <63401246+TheBlueBanana@users.noreply.github.com>
2020-04-10 14:49:51 +0200

    Movement (1)

---
+++ JamCraft 5/Assets/Scripts/Player/WalkScr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkScr : MonoBehaviour
+{
+    public int speed;
+    public float runMultiplier;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))//It would be better to deffine an axis to the inputs in the edit>project settings>Input>axes
+        {
+            transform.Translate(0,0, speed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0,0, -speed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
