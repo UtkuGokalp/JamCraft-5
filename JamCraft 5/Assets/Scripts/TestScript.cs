6db5f4a728ca073eccad0ab5500f16c1e03e30fd
Blue Banana <63401246+TheBlueBanana@users.noreply.github.com>
2020-04-10 12:16:22 +0200

    please

---
+++ JamCraft 5/Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        print("Hello!"); 
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
