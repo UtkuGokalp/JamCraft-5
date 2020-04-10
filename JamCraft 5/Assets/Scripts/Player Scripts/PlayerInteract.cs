e531e65e6c7fcdf95fe991cd9bed24fda9a99e70
Blue Banana <63401246+TheBlueBanana@users.noreply.github.com>
2020-04-10 17:35:29 +0200

    Interactions

---
+++ JamCraft 5/Assets/Scripts/Player Scripts/PlayerInteract.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    #region Variables
+    private GameObject obj;
+    #endregion
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            switch (obj.tag)
+                //for every type of interaction define what does the script do
+            {
+                case "Door":
+                    //GetComponent<DoorScr>().Use();
+                    break;
+                    //specify eveery action for every kind of interaction
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        obj = col.gameObject;
+    }
+
+}
