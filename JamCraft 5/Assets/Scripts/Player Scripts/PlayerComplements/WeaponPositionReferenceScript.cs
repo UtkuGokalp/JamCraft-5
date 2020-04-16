11309c83b29745541cbdeb5f4a6b1f499fa2d4c7
Blue Banana <63401246+TheBlueBanana@users.noreply.github.com>
2020-04-16 10:26:51 +0200

    Weapons difference applied
    There still are some things to modify with the animations

---
+++ JamCraft 5/Assets/Scripts/Player Scripts/PlayerComplements/WeaponPositionReferenceScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPositionReferenceScript : MonoBehaviour
+{
+    private Transform trans => transform;
+    public Transform Trans
+    {
+        get { return trans; }
+    }
+}
