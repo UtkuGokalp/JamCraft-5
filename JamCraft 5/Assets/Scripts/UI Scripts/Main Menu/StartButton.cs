230710ac02ebcfbeff244f766d800b161f07decc
Utku Gokalp <38686664+UtkuGokalp@users.noreply.github.com>
2020-04-17 11:44:23 +0300

    Camera following made smoother.

---
+++ JamCraft 5/Assets/Scripts/UI Scripts/Main Menu/StartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Utility.Development;
+using UnityEngine.SceneManagement;
+
+namespace JamCraft5.UI.Main_Menu
+{
+    public class StartButton : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField]
+        private float fadeTime;
+        #endregion
+
+        #region StartGame
+        public void StartGame(string sceneName)
+        {
+            FadeSystem.Instance.Fade(fadeTime, () =>
+            {
+                SceneManager.LoadScene(sceneName);
+            });
+        } 
+        #endregion
+    }
+}
