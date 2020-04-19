3d483904e6b9ea65c6e0d1ff689213e6af41652a
Blue Banana <63401246+TheBlueBanana@users.noreply.github.com>
2020-04-19 09:54:29 +0200

    Canvar for crafting done

---
+++ JamCraft 5/Assets/Scripts/UI Scripts/CraftingUIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingUIController : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas canv;
+    [SerializeField]
+    private Canvas mainCanv;//The canvas of the life, weapons...
+    private bool active = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CrafSpace") && Input.GetKeyDown(KeyCode.E))
+        {
+            canv.enabled = true;
+            mainCanv.GetComponent<Blacker>().blacker = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            canv.enabled = false;
+            mainCanv.GetComponent<Blacker>().blacker = true;
+        }
+    }
+}
