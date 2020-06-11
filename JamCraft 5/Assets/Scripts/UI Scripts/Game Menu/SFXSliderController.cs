aec4f7f1dffa8a977dabe518fa31c26599bed0c6
Utku Gokalp <38686664+UtkuGokalp@users.noreply.github.com>
2020-06-11 23:07:00 +0300

    Music and SFX slider implemented. (UI is temporary)

---
+++ JamCraft 5/Assets/Scripts/UI Scripts/Game Menu/SFXSliderController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace JamCraft5.UI.Game
+{
+    public class SFXSliderController : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField]
+        private AudioMixer sfxMixer;
+        #endregion
+
+        #region ChangeSFXLevel
+        public void ChangeSFXLevel(float volume)
+        {
+            sfxMixer.SetFloat("SFX Volume", volume);
+        }
+        #endregion
+    }
+}
